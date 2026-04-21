8bcae4c91641fca89fd620402f715efbd678088b
Anbelin <huihuang_yw@foxmail.com>
2026-04-21 18:24:32 +0800

    feat:引入postcss postcss-prefix-selector插件以在 Vite 构建/转换阶段主动加vue3子应用样式选择器前缀，解决vue3样式污染问题

--- sub-vue3-a/vite.config.js
+++ sub-vue3-a/vite.config.js
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import qiankun from 'vite-plugin-qiankun'
+import prefixer from 'postcss-prefix-selector'
+
+const APP_NAME = 'sub-vue3-a'
+const SCOPE = `div[data-qiankun~="${APP_NAME}"]`
 
 export default defineConfig({
   plugins: [
     vue(),
-    qiankun('sub-vue3-a', { useDevMode: true }),
+    qiankun(APP_NAME, { useDevMode: true }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        prefixer({
+          prefix: SCOPE,
+          transform(prefix, selector, prefixedSelector) {
+            if (/^(html|:root|body)\b/.test(selector)) {
+              return selector.replace(/^(html|:root|body)/, prefix)
+            }
+            return prefixedSelector
+          },
+        }),
+      ],
+    },
+  },
   server: {
     port: 8082,
     cors: true,
